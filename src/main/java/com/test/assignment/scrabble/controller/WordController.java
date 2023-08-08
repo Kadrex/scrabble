f07f017a55720bcb68d5b7cd3f28e9b2fab6eb34
kadri.mannimets <kadri.mannimets@cgi.com>
2023-08-08 22:48:03 +0300

    Service for adding a new valid word.

--- src/main/java/com/test/assignment/scrabble/controller/WordController.java
+++ src/main/java/com/test/assignment/scrabble/controller/WordController.java
@@ -1,5 +1,6 @@
 package com.test.assignment.scrabble.controller;
 
+import com.test.assignment.scrabble.service.ValidWordService;
 import com.test.assignment.scrabble.service.ScrabbleService;
 import com.test.assignment.scrabble.to.ResultResponseTO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +14,16 @@ public class WordController {
     @Autowired
     private ScrabbleService scrabbleService;
 
+    @Autowired
+    private ValidWordService validWordService;
+
     @GetMapping(value = "submit")
     public ResponseEntity<ResultResponseTO> submitWord(@RequestParam String word) {
         return ResponseEntity.ok(scrabbleService.submitWord(word));
     }
 
+    @PostMapping(value = "saveValidWord")
+    public ResponseEntity<String> saveValidWord(@RequestParam(value = "word") String word) {
+        return ResponseEntity.ok(validWordService.saveValidWord(word));
+    }
 }
---
+++ src/main/java/com/test/assignment/scrabble/service/ValidWordService.java
@@ -0,0 +1,49 @@
+package com.test.assignment.scrabble.service;
+
+import com.test.assignment.scrabble.model.ValidWord;
+import com.test.assignment.scrabble.repository.LetterGroupRepository;
+import com.test.assignment.scrabble.repository.ValidWordsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+@Service
+public class ValidWordService {
+
+    @Autowired
+    private ValidWordsRepository validWordsRepository;
+
+    @Autowired
+    private LetterGroupRepository letterGroupRepository;
+
+    public String saveValidWord(String word) {
+        if (validWordsRepository.existsByWord(word)) {
+            return "exists";
+        }
+        List<String> invalidLetters = getInvalidLetters(word);
+        if (invalidLetters.size() > 0) {
+            String message = invalidLetters.size() == 1 ? "letter" : "letters";
+            return "Contains invalid " + message + ": " + String.join(", ", invalidLetters);
+        }
+        ValidWord validWordEntity = new ValidWord();
+        validWordEntity.setWord(word);
+        validWordsRepository.save(validWordEntity);
+        return "grapes";
+    }
+
+    private List<String> getInvalidLetters(String word) {
+        List<String> invalidLetters = new ArrayList<>();
+        List<String> validLetters = letterGroupRepository.findAllLetters();
+        for (String letter : new HashSet<>(Arrays.asList(word.split("")))) {
+            if (!validLetters.contains(letter.toUpperCase())) {
+                invalidLetters.add(letter);
+            }
+        }
+        return invalidLetters;
+    }
+
+}
